f552b254e26cc99ebb32481e72543972a44115c1
Dorokhovich Mikhail <mikhail.dorokhovich@creatopy.com>
2021-08-12 20:54:58 +0300

    comment issue

--- src/mappers/product.mapper.ts
+++ src/mappers/product.mapper.ts
@@ -1,6 +1,6 @@
 import { ProductDocument } from '../models/product.model';
 import { JOB_STATUSES } from '../interfaces';
-import { Schema } from 'valivar';
+// import { Schema } from 'valivar';
 
 export type ProductDTOType = {
   id: string | number;
@@ -15,64 +15,64 @@ export type ProductDTOType = {
   status: JOB_STATUSES;
 };
 
-export const ProductDTOSchema = (scope?: string) =>
-  new Schema({
-    id: {
-      type: String,
-    },
-    title: {
-      type: String,
-      required: true,
-    },
-    url: {
-      type: String,
-      required: !!scope,
-    },
-    categoryId: {
-      type: String,
-      required: true,
-    },
-    properties: {
-      type: Array,
-    },
-    code: {
-      type: String,
-      required: !!scope,
-    },
-    images: {
-      type: Array,
-      required: true,
-      each: {
-        type: String,
-      },
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    subheader: {
-      type: String,
-      required: true,
-    },
-    price: {
-      type: String,
-      required: true,
-    },
-    previousPrice: {
-      type: String,
-    },
-    scope: {
-      type: Number,
-    },
-    inStock: {
-      type: Boolean,
-      required: true,
-    },
-    slug: {
-      type: String,
-      required: true,
-    },
-  });
+// export const ProductDTOSchema = (scope?: string) =>
+//   new Schema({
+//     id: {
+//       type: String,
+//     },
+//     title: {
+//       type: String,
+//       required: true,
+//     },
+//     url: {
+//       type: String,
+//       required: !!scope,
+//     },
+//     categoryId: {
+//       type: String,
+//       required: true,
+//     },
+//     properties: {
+//       type: Array,
+//     },
+//     code: {
+//       type: String,
+//       required: !!scope,
+//     },
+//     images: {
+//       type: Array,
+//       required: true,
+//       each: {
+//         type: String,
+//       },
+//     },
+//     description: {
+//       type: String,
+//       required: true,
+//     },
+//     subheader: {
+//       type: String,
+//       required: true,
+//     },
+//     price: {
+//       type: String,
+//       required: true,
+//     },
+//     previousPrice: {
+//       type: String,
+//     },
+//     scope: {
+//       type: Number,
+//     },
+//     inStock: {
+//       type: Boolean,
+//       required: true,
+//     },
+//     slug: {
+//       type: String,
+//       required: true,
+//     },
+//   });
 
 export const mapToDTO = (
   product: ProductDocument,
@@ -93,15 +93,16 @@ export const mapToDTO = (
     previousPrice: product.previousPrice,
     status: product.status,
   };
-  const errors = ProductDTOSchema(product.scope).validate(product.toJSON());
+  // const errors = ProductDTOSchema(product.scope).validate(product.toJSON());
   return {
     product: productDTO,
     ...(includeErrors
       ? {
-          errors: errors.map(({ message, path }) => ({
-            message,
-            path,
-          })),
+          errors: [],
+          // errors: errors.map(({ message, path }) => ({
+          //   message,
+          //   path,
+          // })),
         }
       : {}),
   };
